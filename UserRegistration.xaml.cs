dbb33ed3ac81721d73d856f6b9659e6bc5198d9b
23krychkovska.s@nltu.lviv.ua <23krychkovska.s@nltu.lviv.ua>
2025-12-07 23:00:28 +0200

    форми

---
+++ UserRegistration.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using MySql.Data.MySqlClient;
+
+namespace t
+{
+    public partial class UserRegistration : Window
+    {
+        
+        public UserRegistration()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
+   
